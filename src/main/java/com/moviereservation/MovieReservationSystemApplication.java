16329b78b818bd41e814f87d16025de43869ae70
ys200209 <lys200209@naver.com>
2023-03-15 15:11:30 +0900

    feat(movie): Init Movie Reservation System Project

---
+++ src/main/java/com/moviereservation/MovieReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.moviereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moviereservation/MovieReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
