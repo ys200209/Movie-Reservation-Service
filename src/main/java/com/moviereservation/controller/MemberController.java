b8c60cc03d7be5e05de0c14da0b898c39d014ff9
Lee HyunJy <lhj980424@gmail.com>
2023-04-06 11:43:37 +0900

    feat : add test logic for login

--- src/main/java/com/moviereservation/controller/MemberController.java
+++ src/main/java/com/moviereservation/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.moviereservation.controller;
 import com.moviereservation.domain.Login;
 import com.moviereservation.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +19,21 @@ public class MemberController {
     @Autowired
     MemberService memberService;
 
+
+
     @GetMapping("/login")
     public void login(){
 
     }
 
     @GetMapping("/check")
-    public String check(){
+    public String check(Model model){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetails userDetails = (UserDetails) principal;
+        String username = userDetails.getUsername();
+        String password = userDetails.getPassword();
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
         return "/member/check";
     }
 
--- src/test/java/com/moviereservation/repository/MemberRepositoryImplTest.java
+++ src/test/java/com/moviereservation/repository/MemberRepositoryImplTest.java
@@ -3,23 +3,55 @@ package com.moviereservation.repository;
 import com.moviereservation.domain.Login;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class MemberRepositoryImplTest {
-    @Autowired
-    PasswordEncoder passwordEncoder;
+
 
     @Autowired
     MockMvc mockMvc;
 
+    /*@Test
+    public void login() throws Exception {
+
+        mockMvc.perform(formLogin("/member/login").user("reqw").password("reqw")).andExpect(authenticated());
+    }*/
+
+    //WithMockUser의 접근권한 테스트
     @Test
-    public void login(){
-        Login login = new Login("reqw", passwordEncoder.encode("reqw"));
-        mockMvc.perform(post("/member/login").content())
+    //유저정보 설정
+    @WithMockUser(username="reqw", password="reqw", roles="USER")
+    public void testCheckEndpointAccessAllowed() throws Exception {
+        mockMvc.perform(get("/member/check"))
+                .andExpect(status().isOk());
+    }
+    //익명유저 = 로그인X 유저의 접근 테스트
+    @Test
+    @WithAnonymousUser
+    public void testCheckEndpointAccessDeniedForAnonymousUser() throws Exception {
+        mockMvc.perform(get("/member/check"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    //role이 ADMIN인 유저의 접근테스트
+    @Test
+    @WithMockUser(roles="ADMIN")
+    public void testCheckEndpointAccessDeniedForNonUser() throws Exception {
+        mockMvc.perform(get("/member/check"))
+                .andExpect(status().isOk());
     }
 
 }
