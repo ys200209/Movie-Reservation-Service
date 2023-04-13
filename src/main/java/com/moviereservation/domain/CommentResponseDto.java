6d2406e497c9cfccfc2a296eab8b41b41769e240
jung jongwan <whddhks71@gmail.com>
2023-04-13 11:08:08 +0900

    feat(movie): commit detail page

--- src/main/java/com/moviereservation/domain/CommentResponseDto.java
+++ src/main/java/com/moviereservation/domain/CommentResponseDto.java
@@ -1,16 +1,30 @@
 package com.moviereservation.domain;
 
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+
 
 @Getter
 @RequiredArgsConstructor
 public class CommentResponseDto {
     private final Long seq;
+
+    @Pattern(regexp ="^(?!.*<script>).*$", flags=Pattern.Flag.CASE_INSENSITIVE)
     private final String content;
+
+
     private final String name;
-    private final Date createAt;
+    private final LocalDateTime createAt;
+
+    public CommentResponseDto(Comment comment){
+        this.seq = comment.getSeq();
+        this.content = comment.getContent();
+        this.name = comment.getName();
+        this.createAt = comment.getCreateAt().toLocalDateTime();
+    }
+
 }
