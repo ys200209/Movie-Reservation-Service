fbf4b6ed0de06e4e0087e177fff7ab2568e10ef8
Jihyeon Song <thdwlgus99@naver.com>
2023-05-01 23:10:19 +0900

    feat(movie): add login, logout, regist

---
+++ src/main/java/com/moviereservation/Member.java
@@ -0,0 +1,11 @@
+package com.moviereservation;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Member {
+    private final String name;
+
+}
