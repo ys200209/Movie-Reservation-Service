cf88fced628a6a35252a1486dec7fa93c9eb6e88
operationF <127175623+operationF@users.noreply.github.com>
2023-05-03 22:00:11 +0900

    잘못된 파일 수정

--- src/main/java/com/moviereservation/domain/detail/comment/AddCommentDto.java
+++ src/main/java/com/moviereservation/domain/detail/comment/AddCommentDto.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@NoArgsConstructor(force = true)
 public class AddCommentDto {
     private final String memberId;
     private final String content;
