b05ae7e81ceb1bbfb3a7bcead6699ba048faec5d
ys200209 <lys200209@naver.com>
2023-05-03 15:42:38 +0900

    feat(register): add Register DTO Validator
    validate id, password, gender, birth, name, phoneNumber

--- src/main/java/com/moviereservation/domain/register/controller/RegisterController.java
+++ src/main/java/com/moviereservation/domain/register/controller/RegisterController.java
@@ -2,9 +2,11 @@ package com.moviereservation.domain.register.controller;
 
 import com.moviereservation.domain.register.RegisterService;
 import com.moviereservation.domain.register.controller.dto.MemberRegisterDto;
+import com.moviereservation.domain.register.utils.validator.RegisterValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class RegisterController {
     private final RegisterService service;
+    private final RegisterValidator validator;
 
     @GetMapping("/register")
     public String save(Model model) {
@@ -24,7 +27,12 @@ public class RegisterController {
     }
 
     @PostMapping("/register")
-    public String save(@ModelAttribute("member") MemberRegisterDto member) {
+    public String save(@ModelAttribute("member") MemberRegisterDto member, Errors errors) {
+        validator.validate(member, errors);
+        if (errors.hasErrors()) {
+            return "member/register";
+        }
+
         service.save(member);
         return "redirect:/member/login";
     }
